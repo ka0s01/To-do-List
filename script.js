328f6b6cb84aebc8763812ff9cae039f64682e7c
Aaron <varghese.aaron13@gmail.com>
2026-02-06 09:35:05 +0530

    refactor : todo app architecture, delegated delete function

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
 
-let todos= []
+
+let state = {
+    todos : [],
+    completed: [],
+    theme : "light",
+
+};
 function addTask(){
     let task = document.getElementById("task-name").value ;
     let date = document.getElementById("task-date").value;
-    todos.push({task_name : task,task_date: date});
+    state.todos.push({task_name : task,task_date: date, completed: false});
      
+    document.getElementById("task-name").value = "";
+    document.getElementById("task-date").value = "";
+    render_card();
+}
 
+let listEl = document.getElementById("lists");
+function render_card(){
+    listEl.innerHTML=``;
+    state.todos.forEach((todo,i) => {
+        if(!todo.completed){
+            const task_card = document.createElement("div");
+            task_card.className = "task-card";
+
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.dataset.index = i;
+            checkbox.checked = todo.completed;
+
+            const text = document.createElement("span");
+            text.textContent = `${i}.  ${todo.task_name} - ${todo.task_date}`;
+
+            const dltBtn = document.createElement("button");
+            dltBtn.textContent = "Delete";
+            dltBtn.dataset.action = "delete";
+            dltBtn.dataset.index = i;
+            task_card.append(text,checkbox,dltBtn);
+            listEl.append(task_card);
+        }
+        
+    });
+
+}
+listEl.addEventListener("click", handleClick);
+
+function handleClick(e){
+    if(e.target.dataset.action == "delete"){
+        const idx = e.target.dataset.index;
+        deleteTask(idx);
+    }
+}
+
+
+function deleteTask(idx){
+    state.todos.splice(idx,1);
     render_card();
 }
 
 
-function render_card(){
-    list = document.getElementById("lists");
-    list.innerHTML= "";
-    for (let i = 0;i<todos.length;i++){
-        list.innerHTML+=` 
-        <div class="task-card" > 
-            ${i+1}.
-            ${todos[i].task_name} due on ${todos[i].task_date}
-            <input type="checkbox" class="completed"></input>
-            <button onclick="deleteTask(${i})">Delete</button>
-        
-        `
-    }
 
-    document.getElementById("task-name").value = "";
-    document.getElementById("task-date").value = "";
-}
 
-function deleteTask(i){
-    list = document.getElementById("lists");
-    list.innerHTML= "";
-    todos.splice(i,1);
-    for (let j = 0;j<todos.length;j++){
-        list.innerHTML+=` 
-        <div class="task-card" > 
-            ${j+1}.
-            ${todos[j].task_name} due on ${todos[j].task_date}
-            <input type="checkbox" class="completed"></input>
-            <button onclick="deleteTask(${i})">Delete</button>
-        
-        `
-    }
-}
 
